1bca9cd399206c0607a48c84c4bb591442d8f6b0
Nathan Magdalera <nathanmagdalera@gmail.com>
2024-07-31 18:44:26 -0700

    add basic physics an ai behavior

--- ball.gd
+++ ball.gd
@@ -1,8 +1,16 @@
 extends RigidBody2D
 
+@export var velocity : Vector2
+@export var collision_coords : Vector2
+
 # Initial speed of the ball
-var initial_speed = 50
-var velocity : Vector2
+var initial_speed = 100
+
+# Offset for ball spawn on y-axis
+var y_offset = 100
+
+# Used to determine where it spawns
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 # Ideally we will instantiate the ball on the start of a game and a few seconds
@@ -11,12 +19,17 @@ var velocity : Vector2
 # Once someone scores, the ball will go towards the person who was scored against
 # to give them the first hit
 func _ready():
+	var screen_size = get_viewport_rect().size
 	
+	# initial position
+	position = Vector2(screen_size.x / 2, rng.randf_range(0 + y_offset, screen_size.y - y_offset))
+	
+	var direction = 0
 	# if the player scored...go left
 	# if the ai scored...go right
 	
 	# both checks failed, so give a random velocity
-	velocity = Vector2(-100, 100)
+	velocity = Vector2(direction, rng.randf_range(0, 100))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,6 +38,8 @@ func _process(delta):
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		var reflect = collision.get_remainder().bounce(collision.get_normal())
-		velocity = velocity.bounce(collision.get_normal())
+		collision_coords = Vector2(position.x, position.y)
+		var collision_normal = collision.get_normal()
+		var reflect = collision.get_remainder().bounce(collision_normal)
+		velocity = velocity.bounce(collision_normal)
 		move_and_collide(reflect)
--- enemy_paddle.gd
+++ enemy_paddle.gd
@@ -1,28 +1,68 @@
 extends RigidBody2D
 
-signal hit
-
 @export var speed = 400
 
+# Shorthand for doing the work inside _ready() function
+@onready var paddle_size = get_node("CollisionShape2D").shape.size
+
 var screen_size
+var ball
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(paddle_size)
 	# Returns a vector
 	screen_size = get_viewport_rect().size
+	
+	# Get reference to the ball
+	# Should only do this on a new game start
+	ball = get_parent().get_node("Ball")
 	
 	# starting position
 	var y_position = screen_size.y
 	var x_position = screen_size.x - 50
 	position = Vector2(x_position, y_position / 2)
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
 func _physics_process(delta):
-	# check if the ball is in the arena
-	# if it is, follow its y position
+	var velocity = Vector2.ZERO
 	
-	pass
+	# check if the ball is in the arena
+	if (ball): 
+		var current_position = position.y
+		
+		# middle of the paddle should be 
+		# near the y-intercept
+		
+		# we know the velocity
+		if (ball.velocity.x < 0):
+			# on top side of screen
+			if (current_position < (screen_size.y / 2) + (paddle_size.y / 10)):
+				velocity.y +=1
+			# bottom side of the screen
+			if (current_position > (screen_size.y / 2) - (paddle_size.y / 10)):
+				velocity.y -= 1
+		elif ball.velocity.x > 0:
+			
+			# determine y_intercept
+			var slope = ball.velocity.y / ball.velocity.x
+			var x_diff = position.x - ball.collision_coords.x
+			var y_intercept = x_diff * slope + ball.collision_coords.y
+			
+			# want the y_intercept be between the range of the paddle size
+			# go down
+			
+			# above the y_intercept
+			if current_position < y_intercept + (paddle_size.y / 10):
+				velocity.y += 1
+			# go up
+			if current_position > y_intercept - (paddle_size.y / 10):
+				velocity.y -= 1
+				
+		if (velocity.length() > 0):
+			velocity = velocity.normalized() * speed
+	
+	move_and_collide(velocity * delta)
